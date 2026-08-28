a73299d4049390c0c9811ced27a24c2274c2e058
ALAIZRY <alyzrymhmd700@gmail.com>
2026-08-29 02:41:12 +0300

    Enable WebView printing, permissions and PDF download bridge

--- android/app/src/main/java/com/gard/app/MainActivity.java
+++ android/app/src/main/java/com/gard/app/MainActivity.java
@@ -1,5 +1,28 @@
 package com.gard.app;
 
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // تفعيل أذونات الكاميرا والطباعة والملفات داخل الـ WebView
+        WebView webView = this.bridge.getWebView();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowContentAccess(true);
+        
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+    }
+}
